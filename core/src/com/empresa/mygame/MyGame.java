36e048408ef8253161cabd8ba6b040962b3c05ad
Erik Estrada <erikestradaf21@gmail.com>
2021-08-18 20:04:41 -0300

    Inicio con libGDX v1.0.0

---
+++ core/src/com/empresa/mygame/MyGame.java
@@ -0,0 +1,41 @@
+package com.empresa.mygame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGame extends Game/*ApplicationAdapter*/ {
+	//TODO "extends Game" (xq cambiar?) no paso nada aparentemente
+	SpriteBatch batch;
+	Texture img;
+	
+	public MyGame() {
+		System.out.println("constructor");//debugear
+	}
+	
+	@Override
+	public void create () {// se ejecuta una vez al abrir el juego, bueno para inicializar
+		System.out.println("create");
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {// se ejecuta todo el tiempo, como en bucle y ejecutado paralelamente con el programa
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		// saca/limpia recursos q puse en memoria como img, sonidos, etc
+		//(libera recursos en memoria q ya no se usen)
+		batch.dispose();
+		img.dispose();
+	}
+	//TODO investigar estructura basica de como funciona el libGDX
+}
